8fb0f419a859ecb3e20175a74c208ae7201ffc90
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-02 14:24:28 -0800

    got passing Home test

--- src/components/FilmList/FilmList/FilmList.jsx
+++ src/components/FilmList/FilmList/FilmList.jsx
@@ -5,14 +5,19 @@ import { Link } from 'react-router-dom';
 export default function FilmList({ films }) {
   return (
     <div className="container">
-      {films.map(({ id, title, image }) => (
-        <div className="imgCard" key={id}>
-          <h4 className="title">{title}</h4>
-          <Link key={id} to={`${id}`}>
-            <img className="listImg" src={image}></img>
-          </Link>
-        </div>
-      ))}
+      <ul>
+        {films.map(({ id, title, image }) => (
+          <div className="imgCard" key={id}>
+            <li>
+              <h4 className="title">{title}</h4>
+            </li>
+
+            <Link to={`${id}`}>
+              <img className="listImg" src={image}></img>
+            </Link>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/view/Home/Home.test.jsx
+++ src/view/Home/Home.test.jsx
@@ -3,10 +3,11 @@ import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import Home from './Home';
 import { mockMovieList } from '../../__mocks__/TestData';
+import { MemoryRouter } from 'react-router-dom';
 
 const server = setupServer(
   rest.get(`https://ghibliapi.herokuapp.com/films`, (req, res, ctx) => {
-    return res(ctx.json([mockMovieList]));
+    return res(ctx.json(mockMovieList));
   })
 );
 
@@ -14,11 +15,15 @@ beforeAll(() => server.listen());
 afterAll(() => server.listen());
 
 test('Should render Home page', async () => {
-  render(<Home />);
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
 
-  const title = await screen.findByRole('heading');
   const heading = screen.getByRole('heading');
-
-  expect(title).toBeInTheDocument();
   expect(heading).toBeInTheDocument();
+
+  const filmList = await screen.findAllByRole('listitem');
+  expect(filmList).toHaveLength(22);
 });
