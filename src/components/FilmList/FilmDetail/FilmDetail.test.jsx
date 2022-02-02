10ca04aad25d11d99bf334985f8661a868541b7e
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-02 15:02:06 -0800

    got a passing details test

--- src/components/FilmList/FilmDetail/FilmDetail.test.jsx
+++ src/components/FilmList/FilmDetail/FilmDetail.test.jsx
@@ -1,42 +1,53 @@
 import { render, screen } from '@testing-library/react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
+import { MemoryRouter } from 'react-router-dom';
 import FilmDetail from './FilmDetail';
 
-const ponyo = [
-  {
-    id: '758bf02e-3122-46e0-884e-67cf83df1786',
-    title: 'Ponyo',
-    original_title: '崖の上のポニョ',
-    original_title_romanised: 'Gake no ue no Ponyo',
-    image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/mikKSEdk5kLhflWXbp4S5mmHsDo.jpg',
-    movie_banner: 'https://image.tmdb.org/t/p/original/6a1qZ1qat26mAIK3Lq8iYdGpyHm.jpg',
-    description:
-      "The son of a sailor, 5-year old Sosuke lives a quiet life on an oceanside cliff with his mother Lisa. One fateful day, he finds a beautiful goldfish trapped in a bottle on the beach and upon rescuing her, names her Ponyo. But she is no ordinary goldfish. The daughter of a masterful wizard and a sea goddess, Ponyo uses her father's magic to transform herself into a young girl and quickly falls in love with Sosuke, but the use of such powerful sorcery causes a dangerous imbalance in the world. As the moon steadily draws nearer to the earth and Ponyo's father sends the ocean's mighty waves to find his daughter, the two children embark on an adventure of a lifetime to save the world and fulfill Ponyo's dreams of becoming human.",
-    director: 'Hayao Miyazaki',
-    producer: 'Toshio Suzuki',
-    release_date: '2008',
-    running_time: '100',
-    rt_score: '92',
-    people: ['https://ghibliapi.herokuapp.com/people/'],
-    species: ['https://ghibliapi.herokuapp.com/species/af3910a6-429f-4c74-9ad5-dfe1c4aa04f2'],
-    locations: ['https://ghibliapi.herokuapp.com/locations/'],
-    vehicles: ['https://ghibliapi.herokuapp.com/vehicles/'],
-    url: 'https://ghibliapi.herokuapp.com/films/758bf02e-3122-46e0-884e-67cf83df1786',
-  },
-];
+const castleInSky = {
+  id: '2baf70d1-42bb-4437-b551-e5fed5a87abe',
+  title: 'Castle in the Sky',
+  original_title: '天空の城ラピュタ',
+  original_title_romanised: 'Tenkū no shiro Rapyuta',
+  image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg',
+  movie_banner: 'https://image.tmdb.org/t/p/w533_and_h300_bestv2/3cyjYtLWCBE1uvWINHFsFnE8LUK.jpg',
+  description:
+    "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
+  director: 'Hayao Miyazaki',
+  producer: 'Isao Takahata',
+  release_date: '1986',
+  running_time: '124',
+  rt_score: '95',
+  people: [
+    'https://ghibliapi.herokuapp.com/people/598f7048-74ff-41e0-92ef-87dc1ad980a9',
+    'https://ghibliapi.herokuapp.com/people/fe93adf2-2f3a-4ec4-9f68-5422f1b87c01',
+    'https://ghibliapi.herokuapp.com/people/3bc0b41e-3569-4d20-ae73-2da329bf0786',
+    'https://ghibliapi.herokuapp.com/people/40c005ce-3725-4f15-8409-3e1b1b14b583',
+    'https://ghibliapi.herokuapp.com/people/5c83c12a-62d5-4e92-8672-33ac76ae1fa0',
+    'https://ghibliapi.herokuapp.com/people/e08880d0-6938-44f3-b179-81947e7873fc',
+    'https://ghibliapi.herokuapp.com/people/2a1dad70-802a-459d-8cc2-4ebd8821248b',
+  ],
+  species: ['https://ghibliapi.herokuapp.com/species/af3910a6-429f-4c74-9ad5-dfe1c4aa04f2'],
+  locations: ['https://ghibliapi.herokuapp.com/locations/'],
+  vehicles: ['https://ghibliapi.herokuapp.com/vehicles/4e09b023-f650-4747-9ab9-eacf14540cfb'],
+  url: 'https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe',
+};
 
-const server = setupServer(
-  rest.get(`https://ghibliapi.herokuapp.com/films/`, (req, res, ctx) => {
-    return res(ctx.json([ponyo]));
-  })
-);
-beforeAll(() => server.listen());
-afterAll(() => server.listen());
+// const server = setupServer(
+//   rest.get(`https://ghibliapi.herokuapp.com/films`, (req, res, ctx) => {
+//     return res(ctx.json(castleInSky));
+//   })
+// );
 
-test.skip('should be able to go back to home page', async () => {
-  render(<FilmDetail />);
+// beforeAll(() => server.listen());
+// afterAll(() => server.listen());
 
-  const backBtn = screen.getByRole('button');
-  expect(backBtn).toBeInTheDocument();
+test.only('should be able to render details page', async () => {
+  render(
+    <MemoryRouter>
+      <FilmDetail films={castleInSky} />
+    </MemoryRouter>
+  );
+  screen.debug();
+
+  const heading = screen.getByRole('heading', { name: /castle in the sky/i });
+  expect(heading).toBeInTheDocument();
 });
